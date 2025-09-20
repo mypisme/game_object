bc6637713d6a9fad2add60e1e1067e1596340351
mypisme <1761420261@qq.com>
2025-09-20 14:40:57 +0800

    feat: puzzle game frame

--- puzzlegame/src/main/java/RegisterJFrame.java
+++ puzzlegame/src/main/java/RegisterJFrame.java
@@ -4,7 +4,7 @@ public class RegisterJFrame extends JFrame {
     public RegisterJFrame() {
         this.setSize(488, 500);
 
-        this.setTitle("注册");
+        this.setTitle("注册用户");
         this.setAlwaysOnTop(true);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
